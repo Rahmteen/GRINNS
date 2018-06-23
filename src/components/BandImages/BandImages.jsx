c2ee1f8c23044913557485f2cebc36fa85033606
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2018-06-23 09:15:00 +0100

    s3 integration finished

--- src/components/BandImages/BandImages.jsx
+++ src/components/BandImages/BandImages.jsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react';
 import { Carousel } from 'react-bootstrap';
 
-import BG1 from '../../../assets/images/band/bg1.png';
 import BG2 from '../../../assets/images/band/bg2.png';
 import BG3 from '../../../assets/images/band/bg3.png';
 
@@ -10,10 +9,10 @@ const BandImages = () => {
 		<Fragment>
 			<Carousel style={{ position: 'relative' }} fade={true} className="mb-5" controls={false} indicators={false} interval={5000}>
 				<Carousel.Item>
-					<img className="d-block w-100 mx-auto" src={BG2} alt="Second slide" />
+					<img className="d-block w-100 mx-auto" src={"https://grinns-2021-s3-cfb.s3.amazonaws.com/bg2.png"} alt="Second slide" />
 				</Carousel.Item>
 				<Carousel.Item>
-					<img className="d-block w-100 mx-auto" src={BG3} alt="Third slide" />
+					<img className="d-block w-100 mx-auto" src={"https://grinns-2021-s3-cfb.s3.amazonaws.com/bg3.png"} alt="Third slide" />
 				</Carousel.Item>
 			</Carousel>
 
