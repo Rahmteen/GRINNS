ecba27602e9995791258e724b198518cd7a78639
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2018-07-20 09:01:00 +0100

    updated merch bar and show dates

--- src/components/MerchBar/MerchBar.jsx
+++ src/components/MerchBar/MerchBar.jsx
@@ -1,16 +1,10 @@
-import React, { useState, useEffect, useContext, Fragment } from 'react';
-import { StateContext } from '../../providers/StateProvider';
-import { Link } from 'react-router-dom';
-import { Card, CardDeck, Badge, Container, Col, Row, Image, Button, Navbar, Nav } from 'react-bootstrap';
+import React, { Fragment } from 'react';
+import { Card, CardDeck } from 'react-bootstrap';
 
-import LOGO from '../../../assets/images/GRINNS_LOGO.png';
 import red from '../../../assets/images/merch/redtee.png';
 import block from '../../../assets/images/merch/blocktee.png';
 import ghv from '../../../assets/images/merch/ghv.png';
 import ghc from '../../../assets/images/merch/ghc.png';
-
-import NavBar from '../NavBar';
-import axios from 'axios';
 
 const MerchBar = () => {
 	return (
@@ -79,4 +73,3 @@ const MerchBar = () => {
 
 export default MerchBar;
 
-// LC_ALL=C GIT_COMMITTER_DATE="Mon Mar 5 12:06 2018 +0100" git commit --amend --no-edit --date "Mon Mar 5 12:06 2018 +0100"
