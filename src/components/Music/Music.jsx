c5e79d42b256c07047ee0ebbc920d474a36dbcf2
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2021-08-24 14:01:27 -0700

    updated links to images thru s3 integration

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -1,18 +1,13 @@
 import React, { Fragment } from 'react';
 import { Card, CardDeck } from 'react-bootstrap';
 
-import madeintheshade from '../../../assets/images/mits.png';
-import onmyown from '../../../assets/images/omw.png';
-import tasteofwater from '../../../assets/images/taste.png';
-import reunion from '../../../assets/images/reunion.jpeg';
-
 const MusicBar = () => {
 	return (
 		<Fragment>
 			<CardDeck className="mx-auto text-center d-flex align-content-center music-container">
 			<Card className="p-4" bg={'dark'} style={{ width: '100%' }}>
 					<a href="https://lnk.to/SEARCHLIGHTS">
-						<Card.Img variant="top" src={reunion} />
+						<Card.Img variant="top" src={"https://grinns-2021-s3-cfb.s3.amazonaws.com/music/search.png"} />
 					</a>
 					<Card.Body className="mx-auto">
 						<Card.Title className="text-center text-white youtube-title mb-2 font-roboto mt-1">searchlights</Card.Title>
@@ -20,7 +15,7 @@ const MusicBar = () => {
 				</Card>
 				<Card className="p-4" bg={'dark'} style={{ width: '100%' }}>
 					<a href="https://lnk.to/cadc4tIj">
-						<Card.Img variant="top" src={onmyown} />
+						<Card.Img variant="top" src={"https://grinns-2021-s3-cfb.s3.amazonaws.com/music/omw.png"} />
 					</a>
 					<Card.Body className="mx-auto">
 						<Card.Title className="text-center text-white youtube-title mb-2 font-roboto mt-1">on my own</Card.Title>
@@ -29,7 +24,7 @@ const MusicBar = () => {
 
 				<Card className="p-4" bg={'dark'} style={{ width: '100%' }}>
 					<a href="https://lnk.to/9md2E6ke">
-						<Card.Img variant="top" src={madeintheshade} />
+						<Card.Img variant="top" src={"https://grinns-2021-s3-cfb.s3.amazonaws.com/music/mits.png"} />
 					</a>
 					<Card.Body className="mx-auto">
 						<Card.Title className="text-center text-white youtube-title mb-2 font-roboto mt-1">made in the shade</Card.Title>
@@ -37,7 +32,7 @@ const MusicBar = () => {
 				</Card>
 				<Card className="p-4" bg={'dark'} style={{ width: '100%' }}>
 					<a href="https://lnk.to/J1tFOdKN">
-						<Card.Img variant="top" src={tasteofwater} />
+						<Card.Img variant="top" src={"https://grinns-2021-s3-cfb.s3.amazonaws.com/music/taste.png"} />
 					</a>
 					<Card.Body className="mx-auto">
 						<Card.Title className="text-center text-white youtube-title mb-2 font-roboto mt-1">taste of water</Card.Title>
