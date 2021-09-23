b4d4c50af37a5d3a796f5a4dacf191e389d8f06a
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2021-09-23 15:46:55 -0700

    updated image styling

--- src/components/BandImages/BandImages.jsx
+++ src/components/BandImages/BandImages.jsx
@@ -4,7 +4,7 @@ const BandImages = () => {
 	return (
 		<Fragment>
 			<div className="image-div">
-				<img src="https://grinns-2021-s3-cfb.s3.amazonaws.com/image1.png" className="header-image"></img>
+				<img src="https://grinns-2021-s3-cfb.s3.amazonaws.com/image1.png" className="header-image d-block w-100 mx-auto"></img>
 			</div>
 		</Fragment>
 	);
